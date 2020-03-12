1e43fd5694d0f1381d6fb97ab5132ec872ff8efa
yusufgoh <54039371+yusufgoh@users.noreply.github.com>
2020-03-12 21:36:34 +0700

    Halamandinamis

---
+++ halamandinamis/halaman/home.php
@@ -0,0 +1,4 @@
+<div class="halaman">
+	<h2>Halaman Depan</h2>
+	<p>Selamat datang Pemrograman Web UIB. ini merupakan halaman depan pada demo cara Membuat Halaman Web Dinamis Dengan PHP</p>
+</div>
---
+++ halamandinamis/halaman/tentang.php
@@ -0,0 +1,5 @@
+<div class="halaman">
+	<h2>Tentang Kami</h2>
+	<p>Ini adalah halaman tentang kami</p>
+	<p>Pemrograman Web UIB adalah mata kuliah yang memberikan pelatihan dan pembelajaran mengenai konten-konten berkualitas untuk belajar pemrograman web</p>
+</div>
